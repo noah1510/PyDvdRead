0e4a11cd40d944dd6753130a16b9f4219af67da6
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2024-05-02 12:24:01 +0200

    some reformatting, and bump version target to python3.12

--- setup.py
+++ setup.py
@@ -1,38 +1,33 @@
 import sys
 from distutils.core import setup, Extension
 
-majv = 1
-minv = 1
+majv = 2
+minv = 0
 
-if sys.version_info < (3,):
-	print("This library is only tested with Python 3.4")
-	sys.exit(1)
-
-dvdread4 = Extension(
-	'_dvdread',
-	define_macros = [
-		('MAJOR_VERSION', str(majv)),
-		('MINOR_VERSION', str(minv))
-	],
-        include_dirs = ['/usr/include'],
-	libraries = ['dvdread'],
-	sources = ['src/dvdread.c'],
-	extra_compile_args = ['-std=c99']
+dvdread = Extension(
+    '_dvdread',
+    define_macros=[
+        ('MAJOR_VERSION', str(majv)),
+        ('MINOR_VERSION', str(minv))
+    ],
+    include_dirs=['/usr/include'],
+    libraries=['dvdread'],
+    sources=['src/dvdread.c'],
+    extra_compile_args=['-std=c11']
 )
 
 setup(
-	name = 'dvdread',
-	version = '%d.%d'%(majv,minv),
-	description = 'Python wrapper for libdvdread4',
-	author = 'Colin ML Burnett',
-	author_email = 'cmlburnett@gmail.com',
-	url = "https://github.com/cmlburnett/PyDvdRead",
-	download_url = "https://pypi.python.org/pypi/dvdread",
-	packages = ['dvdread'],
-	ext_modules = [dvdread4],
-	requires = ['crudexml'],
-	classifiers = [
-		'Programming Language :: Python :: 3.4'
-	]
+    name='dvdread',
+    version='%d.%d' % (majv, minv),
+    description='Python wrapper for libdvdread',
+    author='Colin ML Burnett',
+    author_email='cmlburnett@gmail.com',
+    url="https://github.com/cmlburnett/PyDvdRead",
+    download_url="https://pypi.python.org/pypi/dvdread",
+    packages=['dvdread'],
+    ext_modules=[dvdread],
+    requires=['crudexml'],
+    classifiers=[
+        'Programming Language :: Python :: 3.12'
+    ]
 )
-
